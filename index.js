ffc88a5f840610ee8a0e5e899dff281c9b17387b
Antje Witte <antjewitte90@gmail.com>
2023-04-27 10:12:59 +0200

    toggle and button

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // phew… not a lot going on here. Please add some code!
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+const button = document.querySelector('[data-js="button"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+  console.log(bookmark);
+});
+
+button.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+
+  if (answer.classList.contains("card__answer--active")) {
+    button.textContent = "Hide answer";
+  } else {
+    button.textContent = "Show answer";
+  }
+
+  console.log(button);
+});
